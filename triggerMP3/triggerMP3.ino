3116ad59fa8bd7bebac58dcbf506599da14f569b
Mr-Hansen-Orewa <d.hansen@orewacollege.nz>
2022-04-14 13:49:49 +1200

    Update triggerMP3.ino
    updated example code to newer version
    added PIR example code and random number generation
    
    started adjusting code for readability and standardisation

--- triggerMP3/triggerMP3.ino
+++ triggerMP3/triggerMP3.ino
@@ -1,313 +1,235 @@
 /*
- 4-28-2011
- Spark Fun Electronics 2011
- Nathan Seidle
- 
- This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- 
- This example shows how to play a given track (track001.mp3 for example) from the SD card when a GPIO pin goes low. 
- This code monitors A0 through A5. If A0 is pulled low then track 1 plays. If A5 is pulled low then the track # simply advances.
- 
- This code is heavily based on the "MP3 PlayTrack" example code. Please be sure to review it for more comments and gotchas.
- 
- While track is playing, Arduino scans to see if pins change. If pins change, stop playing track, check pins again to get 
- trigger # and then decide what new track to play.
- 
- */
 
-#include <SPI.h>
+  This is an example MP3 trigger sketch for the SparkFun MP3 Shield.
+  Pins 0, 1, 5, 10, A0, A1, A2, A3, and A4 are setup to trigger tracks
+  "track001.mp3", "track002.mp3", etc. on an SD card loaded into
+  the shield. Whenever any of those pins are shorted to ground,
+  their respective track will start playing.
 
-#define TRUE  0
-#define FALSE  1
+  When a new pin is triggered, any track currently playing will
+  stop, and the new one will start.
 
-//Add the SdFat Libraries
-#include <SdFat.h>
-#include <SdFatUtil.h> 
+  A5 is setup to globally STOP playing a track when triggered.
 
-//Create the variables to be used by SdFat Library
-Sd2Card card;
-SdVolume volume;
-SdFile root;
-SdFile track;
+  Much of this code was grabbed from the MP3 Shield Trigger and FilePlayer example
+  included with the SFEMP3Shield library. Major thanks to Jim Lindblom Bill
+  Porter and Michael Flaga, again, for this amazing library!
+*/
 
-//This is the name of the file on the microSD card you would like to play
-//Stick with normal 8.3 nomeclature. All lower-case works well.
-//Note: you must name the tracks on the SD card with 001, 002, 003, etc. 
-//For example, the code is expecting to play 'track002.mp3', not track2.mp3.
-char trackName[] = "track001.mp3";
-int trackNumber = 1;
-int previousTrigger = 1; //This indicates that we've already triggered on 1
+#include <SPI.h>           // SPI library
+#include <SdFat.h>         // SDFat Library
+//#include <SdFatUtil.h>     // SDFat Util Library
+#include <SFEMP3Shield.h>  // Mp3 Shield Library
 
-long lastCheck; //This stores the last millisecond since we had a trigger
+SdFat sd; // Create object to handle SD functions
 
-char errorMsg[100]; //This is a generic array used for sprintf of error messages
+SFEMP3Shield MP3player; // Create Mp3 library object
+// These variables are used in the MP3 initialization to set up
+// some stereo options:
+const byte VOLUME = 0; // MP3 Player volume 0=max, 255=lowest (off)
+const byte MONOMODE = 1;  // Mono setting 0=off, 3=max
 
-int trigger0 = A0;
-int trigger1 = A1;
+/* Pin setup */
+//#define TRIGGER_COUNT 9
+//int triggerPins[TRIGGER_COUNT] = {0, 1, 5, 10, A0, A1, A2, A3, A4};
+//int stopPin = A5; // This pin triggers a track stop.
+//int lastTrigger = 0; // This variable keeps track of which tune is playing
 
-//MP3 Player Shield pin mapping. See the schematic
-#define MP3_XCS 6 //Control Chip Select Pin (for accessing SPI Control/Status registers)
-#define MP3_XDCS 7 //Data Chip Select / BSYNC Pin
-#define MP3_DREQ 2 //Data Request Pin: Player asks for more data
-#define MP3_RESET 8 //Reset is active low
-//Remember you have to edit the Sd2PinMap.h of the sdfatlib library to correct control the SD card.
 
-//VS10xx SCI Registers
-#define SCI_MODE 0x00
-#define SCI_STATUS 0x01
-#define SCI_BASS 0x02
-#define SCI_CLOCKF 0x03
-#define SCI_DECODE_TIME 0x04
-#define SCI_AUDATA 0x05
-#define SCI_WRAM 0x06
-#define SCI_WRAMADDR 0x07
-#define SCI_HDAT0 0x08
-#define SCI_HDAT1 0x09
-#define SCI_AIADDR 0x0A
-#define SCI_VOL 0x0B
-#define SCI_AICTRL0 0x0C
-#define SCI_AICTRL1 0x0D
-#define SCI_AICTRL2 0x0E
-#define SCI_AICTRL3 0x0F
+//PIR
+const byte PIR_AOUT = A0;  // PIR analog output on A0
+const byte PIR_DOUT = 2;   // PIR digital output on D2
+const byte LED_PIN = 13;  // LED to illuminate on motion
+
+const byte PRINT_TIME 100 // Rate of serial printouts
+unsigned long lastPrint = 0; // Keep track of last serial out
+
+
+const byte RMIN = 0;
+const byte RMAX = 5;
 
 void setup() {
-  //Setup the triggers as inputs
-  pinMode(trigger0, INPUT);
-  pinMode(trigger1, INPUT);
+  /* Set up all trigger pins as inputs, with pull-ups activated: */
+  //  for (int i = 0; i < TRIGGER_COUNT; i++) {
+  //    pinMode(triggerPins[i], INPUT_PULLUP);
+  //  }
+  //  pinMode(stopPin, INPUT_PULLUP);
 
-  //Enable pullups on triggers
-  digitalWrite(trigger0, HIGH);
-  digitalWrite(trigger1, HIGH);
+  initSD();  // Initialize the SD card
+  initMP3Player(); // Initialize the MP3 Shield
 
-  pinMode(MP3_DREQ, INPUT);
-  pinMode(MP3_XCS, OUTPUT);
-  pinMode(MP3_XDCS, OUTPUT);
-  pinMode(MP3_RESET, OUTPUT);
 
-  digitalWrite(MP3_XCS, HIGH); //Deselect Control
-  digitalWrite(MP3_XDCS, HIGH); //Deselect Data
-  digitalWrite(MP3_RESET, LOW); //Put VS1053 into hardware reset
+  //PIR
+  // Analog and digital pins should both be set as inputs:
+  pinMode(PIR_AOUT, INPUT);
+  pinMode(PIR_DOUT, INPUT);
 
-  Serial.begin(9600); //Use serial for debugging 
-  Serial.println("dino roar code triggered off a PIR");
+  // Configure the motion indicator LED pin as an output
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Turn the LED off
 
-  //Setup SD card interface
-  pinMode(10, OUTPUT);       //Pin 10 must be set as an output for the SD communication to work.
-  if (!card.init(SPI_FULL_SPEED, 9))  Serial.println("Error: Card init"); //Initialize the SD card, shield uses pin 9 for SD CS
-  if (!volume.init(&card)) Serial.println("Error: Volume ini"); //Initialize a volume on the SD card.
-  if (!root.openRoot(&volume)) Serial.println("Error: Opening root"); //Open the root directory in the volume. 
-
-  //We have no need to setup SPI for VS1053 because this has already been done by the SDfatlib
-
-  //From page 12 of datasheet, max SCI reads are CLKI/7. Input clock is 12.288MHz. 
-  //Internal clock multiplier is 1.0x after power up. 
-  //Therefore, max SPI speed is 1.75MHz. We will use 1MHz to be safe.
-  SPI.setClockDivider(SPI_CLOCK_DIV16); //Set SPI bus speed to 1MHz (16MHz / 16 = 1MHz)
-  SPI.transfer(0xFF); //Throw a dummy byte at the bus
-
-  //Initialize VS1053 chip 
-  delay(10);
-  digitalWrite(MP3_RESET, HIGH); //Bring up VS1053
-
-  //Mp3SetVolume(20, 20); //Set initial volume (20 = -10dB) LOUD
-  Mp3SetVolume(40, 40); //Set initial volume (20 = -10dB) Manageable
-  //Mp3SetVolume(80, 80); //Set initial volume (20 = -10dB) More quiet
-
-  //Now that we have the VS1053 up and running, increase the internal clock multiplier and up our SPI rate
-  Mp3WriteRegister(SCI_CLOCKF, 0x60, 0x00); //Set multiplier to 3.0x
-
-  //From page 12 of datasheet, max SCI reads are CLKI/7. Input clock is 12.288MHz. 
-  //Internal clock multiplier is now 3x.
-  //Therefore, max SPI speed is 5MHz. 4MHz will be safe.
-  SPI.setClockDivider(SPI_CLOCK_DIV4); //Set SPI bus speed to 4MHz (16MHz / 4 = 4MHz)
-
-  //MP3 IC setup complete
-
-  trackNumber = 1; //Setup to play track001 by default
-
-  previousTrigger = 255; //Setup the new vs old trigger
-
-// SEED THE RANDOM NUMBER GENERATOR
+  //RANDOM
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
   randomSeed(analogRead(0));
 }
 
-void loop(){
+// All the loop does is continuously step through the trigger
+//  pins to see if one is pulled low. If it is, it'll stop any
+//  currently playing track, and start playing a new one.
+void loop() {
+
+ //RANDOM
+  // print a random number from 0 to 4
+  randNumber = random(RMIN, RMAX);
+  Serial.println(randNumber);
   
-  if(triggerNumber == 0){ //TODO: put the PIR trigger here 
-    trackNumber = random(1, 5); // gives track numbers 1 - 4 excluding 5
-  }
-
-  //Let's play a track of a given number
-  sprintf(trackName, "track%03d.mp3", trackNumber); //Splice the new file number into this file name
-  playMP3(trackName); //Go play trackXXX.mp3
-}
-
-//checkTriggers checks the level of the various inputs
-//It returns 255 if no trigger is activated
-//Otherwise, it returns the number of the first trigger it sees
-int checkTriggers(void) {
-
-#define DEBOUNCE  100
-
-  int foundTrigger = 255;
-
-  //Once a trigger is activated, we don't want to trigger on it perpetually
-  //But after 3 seconds, reset the previous trigger number
-  if( (previousTrigger != 255) && (millis() - lastCheck) > 3000) {
-    lastCheck = millis();
-    previousTrigger = 255;
-    Serial.println("Previous trigger reset");
-  }
-
-  //Debounce button, then check again
-  if(digitalRead(trigger0) == LOW){ 
-    delay(DEBOUNCE); 
-    if(digitalRead(trigger0) == LOW) foundTrigger = 0;
-  }
-  else if(digitalRead(trigger1) == LOW ){ 
-    delay(DEBOUNCE); 
-    if(digitalRead(trigger1) == LOW) foundTrigger = 1;
-  }
-  else if(digitalRead(trigger2) == LOW){ 
-    delay(DEBOUNCE); 
-    if(digitalRead(trigger2) == LOW) foundTrigger = 2;
-  }
-  else if(digitalRead(trigger3) == LOW){ 
-    delay(DEBOUNCE); 
-    if(digitalRead(trigger3) == LOW) foundTrigger = 3;
-  }
-  else if(digitalRead(trigger4) == LOW){ 
-    delay(DEBOUNCE); 
-    if(digitalRead(trigger4) == LOW) foundTrigger = 4;
-  }
-  else if(digitalRead(trigger5) == LOW){ 
-    delay(DEBOUNCE); 
-    if(digitalRead(trigger5) == LOW) foundTrigger = 5;
-  }
-
-  if(foundTrigger != previousTrigger){ //We've got a new trigger!
-    previousTrigger = foundTrigger; 
-
-    Serial.print("T");
-    Serial.println(foundTrigger, DEC);
-
-    return(foundTrigger);
-  }
-  else
-    return(255); //No triggers pulled low (activated)
-}
-
-//PlayMP3 plays a given file name
-//It pulls 32 byte chunks from the SD card and throws them at the VS1053
-//We monitor the DREQ (data request pin). If it goes low then we determine if
-//we need new data or not. If yes, pull new from SD card. Then throw the data
-//at the VS1053 until it is full.
-void playMP3(char* fileName) {
-
-  if (!track.open(&root, fileName, O_READ)) { //Open the file in read mode.
-    sprintf(errorMsg, "Failed to open %s", fileName);
-    Serial.println(errorMsg);
-    return;
-  }
-
-  sprintf(errorMsg, "Playing track %s", fileName);
-  Serial.println(errorMsg);
-
-  uint8_t mp3DataBuffer[32]; //Buffer of 32 bytes. VS1053 can take 32 bytes at a go.
-  int need_data = TRUE; 
-
-  while(1) {
-    while(!digitalRead(MP3_DREQ)) { 
-      //DREQ is low while the receive buffer is full
-      //You can do something else here, the buffer of the MP3 is full and happy.
-      //Maybe set the volume or test to see how much we can delay before we hear audible glitches
-
-      //If the MP3 IC is happy, but we need to read new data from the SD, now is a great time to do so
-      if(need_data == TRUE) {
-        if(!track.read(mp3DataBuffer, sizeof(mp3DataBuffer))) { //Try reading 32 new bytes of the song
-          //Oh no! There is no data left to read!
-          //Time to exit
-          break;
-        }
-        need_data = FALSE;
+      lastTrigger = i + 1; // Update lastTrigger variable to current trigger
+      /* If another track is playing, stop it: */
+      if (MP3player.isPlaying()) {
+        MP3player.stopTrack();
       }
+      /* Use the playTrack function to play a numbered track: */
+      uint8_t result = MP3player.playTrack(lastTrigger);
+      // An alternative here would be to use the
+      //  playMP3(fileName) function, as long as you mapped
+      //  the file names to trigger pins.
 
-      //Check to see if we need to bail on this track
-      if(checkTriggers() != 255) { 
-        Serial.println("Exiting MP3!");
-        track.close(); //Close this track!
-        previousTrigger = 255; //Trick the next check into thinking we haven't seen a previous trigger
-        return;
+      if (result == 0) { // playTrack() returns 0 on success
+        // Success
       }
+      else { // Otherwise there's an error, check the code
+        // Print error code somehow, someway
+      }
+ 
+  // After looping through and checking trigger pins, check to
+  //  see if the stopPin (A5) is triggered.
+  if (digitalRead(stopPin) == LOW) {
+    // lastTrigger = 0; // Reset lastTrigger
+    // If another track is playing, stop it.
+    if (MP3player.isPlaying()) {
+      MP3player.stopTrack();
     }
+  }
 
-    if(need_data == TRUE){ //This is here in case we haven't had any free time to load new data
-      if(!track.read(mp3DataBuffer, sizeof(mp3DataBuffer))) { //Go out to SD card and try reading 32 new bytes of the song
-        //Oh no! There is no data left to read!
-        //Time to exit
+  //PIR
+  // Read OUT pin, and set onboard LED to mirror output
+  readDigitalValue();
+
+  // Read A pin, print that value to serial port:
+  printAnalogValue();
+
+ 
+
+}
+/*
+  initSD() initializes the SD card and checks for an error
+*/
+void initSD() {
+  //Initialize the SdCard.
+  if (!sd.begin(SD_SEL, SPI_HALF_SPEED)) {
+    sd.initErrorHalt();
+    Serial.println("Error initialising the SD Card");
+  }
+  if (!sd.chdir("/")) {
+    sd.errorHalt("sd.chdir");
+    Serial.println("Error setting the root directory");
+  }
+}
+
+/*
+  initMP3Player() sets up all of the initialization for the
+  MP3 Player Shield. It runs the begin() function, checks
+  for errors, applies a patch if found, and sets the volume/stero mode
+*/
+void initMP3Player() {
+  byte result = MP3player.begin();
+  if (result != 0) {
+    Serial.println("Error initialising the MP3 Shield");
+  }
+  MP3player.setVolume(VOLUME, VOLUME);
+  MP3player.setMonoMode(MONOMODE);
+}
+
+
+//PIR
+void readDigitalValue()
+{
+  // The OpenPIR's digital output is active high
+  int motionStatus = digitalRead(PIR_DOUT);
+
+  if (motionStatus == HIGH) {
+    Serial.println("Digital motion is HIGH");
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    Serial.println("Digital motion is HIGH");
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
+
+//PIR
+void printAnalogValue()
+{
+  if ( (lastPrint + PRINT_TIME) < millis() )
+  {
+    lastPrint = millis();
+    // Read in analog value:
+    unsigned int analogPIR = analogRead(PIR_AOUT);
+    // Convert 10-bit analog value to a voltage
+    // (Assume high voltage is 5.0V.)
+    float voltage = (float) analogPIR / 1024.0 * 5.0;
+    // Print the reading from the digital pin.
+    // Mutliply by 5 to maintain scale with AOUT.
+    Serial.print(5 * digitalRead(PIR_DOUT));
+    Serial.print(',');    // Print a comma
+    Serial.print(2.5);    // Print the upper limit
+    Serial.print(',');    // Print a comma
+    Serial.print(1.7);    // Print the lower limit
+    Serial.print(',');    // Print a comma
+    Serial.print(voltage); // Print voltage
+    Serial.println();
+  }
+}
+
+void roarChoice(byte choice) {
+  // The OpenPIR's digital output is active high
+  int motionStatus = digitalRead(PIR_DOUT);
+
+  if (motionStatus == HIGH) {
+    Serial.println("Roar");
+    switch (choice) {
+      case 0:
+        Serial.println("Roar 0");
+        //more track playing code here
         break;
-      }
-      need_data = FALSE;
+      case 1:
+        Serial.println("Roar 1");
+        //more track playing code here
+        break;
+      case 2:
+        Serial.println("Roar 2");
+        //more track playing code here
+        break;
+      case 3:
+        Serial.println("Roar 3");
+        //more track playing code here
+        break;
+      case 4:
+        Serial.println("Roar 4");
+        //more track playing code here
+        break;
+      default:
+        Serial.println("Random picked a roar that doesn't exist");
+        break;
     }
-
-    //Once DREQ is released (high) we now feed 32 bytes of data to the VS1053 from our SD read buffer
-    digitalWrite(MP3_XDCS, LOW); //Select Data
-    for(int y = 0 ; y < sizeof(mp3DataBuffer) ; y++)
-      SPI.transfer(mp3DataBuffer[y]); // Send SPI byte
-
-    digitalWrite(MP3_XDCS, HIGH); //Deselect Data
-    need_data = TRUE; //We've just dumped 32 bytes into VS1053 so our SD read buffer is empty. Set flag so we go get more data
+  } else {
+    Serial.println("don't Roar");
+    //if a track is playing stop it
+    if (MP3player.isPlaying()) {
+      MP3player.stopTrack();
+    }
   }
-
-  while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating transfer is complete
-  digitalWrite(MP3_XDCS, HIGH); //Deselect Data
-
-  track.close(); //Close out this track
-
-  sprintf(errorMsg, "Track %s done!", fileName);
-  Serial.println(errorMsg);
-}
-
-
-//Write to VS10xx register
-//SCI: Data transfers are always 16bit. When a new SCI operation comes in 
-//DREQ goes low. We then have to wait for DREQ to go high again.
-//XCS should be low for the full duration of operation.
-void Mp3WriteRegister(unsigned char addressbyte, unsigned char highbyte, unsigned char lowbyte){
-  while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating IC is available
-  digitalWrite(MP3_XCS, LOW); //Select control
-
-  //SCI consists of instruction byte, address byte, and 16-bit data word.
-  SPI.transfer(0x02); //Write instruction
-  SPI.transfer(addressbyte);
-  SPI.transfer(highbyte);
-  SPI.transfer(lowbyte);
-  while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating command is complete
-  digitalWrite(MP3_XCS, HIGH); //Deselect Control
-}
-
-//Read the 16-bit value of a VS10xx register
-unsigned int Mp3ReadRegister (unsigned char addressbyte){
-  while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating IC is available
-  digitalWrite(MP3_XCS, LOW); //Select control
-
-  //SCI consists of instruction byte, address byte, and 16-bit data word.
-  SPI.transfer(0x03);  //Read instruction
-  SPI.transfer(addressbyte);
-
-  char response1 = SPI.transfer(0xFF); //Read the first byte
-  while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating command is complete
-  char response2 = SPI.transfer(0xFF); //Read the second byte
-  while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating command is complete
-
-  digitalWrite(MP3_XCS, HIGH); //Deselect Control
-
-  int resultvalue = response1 << 8;
-  resultvalue |= response2;
-  return resultvalue;
-}
-
-//Set VS10xx Volume Register
-void Mp3SetVolume(unsigned char leftchannel, unsigned char rightchannel){
-  Mp3WriteRegister(SCI_VOL, leftchannel, rightchannel);
 }
