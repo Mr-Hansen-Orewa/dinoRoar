62d6bfb3c2e95a4c9163526bcfd6b68423a49f96
Mr-Hansen-Orewa <d.hansen@orewacollege.nz>
2022-04-14 14:49:01 +1200

    Update triggerMP3.ino
    redid comments
    set up the mp3 code to close to final state - needs testing

--- triggerMP3/triggerMP3.ino
+++ triggerMP3/triggerMP3.ino
@@ -11,7 +11,7 @@
 SdFat sd; // Create object to handle SD functions
 SFEMP3Shield MP3player; // Create Mp3 library object
 
-const byte VOLUME = 0; // MP3 Player volume 0=max, 255=lowest (off)
+byte volume = 128; // MP3 Player volume 0=max, 255=lowest (off)
 const byte MONOMODE = 1;  // Mono setting 0=off, 3=max
 
 
@@ -45,47 +45,21 @@ void setup() {
   Serial.println("Setup finished");
 }
 
-// All the loop does is continuously step through the trigger
-//  pins to see if one is pulled low. If it is, it'll stop any
-//  currently playing track, and start playing a new one.
+/*
+  pick a random number, call the roarChoice method, wait for 3 seconds
+ */
 void loop() {
-
-  //RANDOM
   // print a random number from 0 to 4
   randNumber = random(RMIN, RMAX);
-  Serial.println(randNumber);
+  Serial.println("Random number chosen is " + String(randNumber));
 
-  lastTrigger = i + 1; // Update lastTrigger variable to current trigger
-  /* If another track is playing, stop it: */
-  if (MP3player.isPlaying()) {
-    MP3player.stopTrack();
-  }
-  /* Use the playTrack function to play a numbered track: */
-  uint8_t result = MP3player.playTrack(lastTrigger);
-  // An alternative here would be to use the
-  //  playMP3(fileName) function, as long as you mapped
-  //  the file names to trigger pins.
-
-  if (result == 0) { // playTrack() returns 0 on success
-    // Success
-  }
-  else { // Otherwise there's an error, check the code
-    // Print error code somehow, someway
-  }
-
-  //PIR
-  // Read OUT pin, and set onboard LED to mirror output
-  readDigitalValue();
-
-  // Read A pin, print that value to serial port:
-  printAnalogValue();
-
-// call the method with the random number
-roarChoice(randNumber);
-
+  roarChoice(randNumber);
+  // wait for the roar to finish
+  delay(3000);
 }
+
 /*
-  initSD() initializes the SD card and checks for an error
+  initialise the SD card, brief error checks
 */
 void initSD() {
   //Initialize the SdCard.
@@ -100,96 +74,98 @@ void initSD() {
 }
 
 /*
-  initMP3Player() sets up all of the initialization for the
-  MP3 Player Shield. It runs the begin() function, checks
-  for errors, applies a patch if found, and sets the volume/stero mode
+  initialise the MP3 player, brief error check, set volume for L and R and mono
 */
 void initMP3Player() {
   byte result = MP3player.begin();
   if (result != 0) {
     Serial.println("Error initialising the MP3 Shield");
+    Serial.println("Error code is " + String(result));
+    /*
+      0 OK
+      1 *Failure of SdFat to initialize physical contact with the SdCard
+      2 *Failure of SdFat to start the SdCard's volume
+      3 *Failure of SdFat to mount the root directory on the volume of the SdCard
+      4 Other than default values were found in the SCI_MODE register.
+      5 SCI_CLOCKF did not read back and verify the configured value.
+      6 Patch was not loaded successfully. This may result in playTrack errors
+    */
   }
   MP3player.setVolume(VOLUME, VOLUME);
   MP3player.setMonoMode(MONOMODE);
 }
 
-
-//PIR
-void readDigitalValue()
-{
-  // The OpenPIR's digital output is active high
-  int motionStatus = digitalRead(PIRDOUT);
-
-  if (motionStatus == HIGH) {
-    Serial.println("Digital motion is HIGH");
-    digitalWrite(LEDPIN, HIGH);
-  } else {
-    Serial.println("Digital motion is HIGH");
-    digitalWrite(LEDPIN, LOW);
-  }
-}
-
-
-//PIR
-void printAnalogValue()
-{
-  if ( (lastPrint + PRINTTIME) < millis() )
-  {
-    lastPrint = millis();
-    // Read in analog value:
-    unsigned int analogPIR = analogRead(PIRAOUT);
-    // Convert 10-bit analog value to a voltage
-    // (Assume high voltage is 5.0V.)
-    float voltage = (float) analogPIR / 1024.0 * 5.0;
-    // Print the reading from the digital pin.
-    // Mutliply by 5 to maintain scale with AOUT.
-    Serial.print(5 * digitalRead(PIRDOUT));
-    Serial.print(',');    // Print a comma
-    Serial.print(2.5);    // Print the upper limit
-    Serial.print(',');    // Print a comma
-    Serial.print(1.7);    // Print the lower limit
-    Serial.print(',');    // Print a comma
-    Serial.print(voltage); // Print voltage
-    Serial.println();
-  }
-}
-
+/*
+  check for motion, set the volume based on the Analog PIR output, call the playTrack method, brief error check
+ */
 void roarChoice(byte choice) {
   // The OpenPIR's digital output is active high
   int motionStatus = digitalRead(PIRDOUT);
 
   if (motionStatus == HIGH) {
-    Serial.println("Roar");
+    Serial.println("Motion detected");
+
+  // MP3 Player volume 0=max, 255=lowest (off)
+  volume = map(analogRead(PIRAOUT), 0, 1023, 255, 0);
+  MP3player.setVolume(volume, volume);
+    
+    Serial.print("choice is ");
     switch (choice) {
       case 0:
         Serial.println("Roar 0");
-        //more track playing code here track000.mp3
+        playTrack("track000.mp3");
         break;
       case 1:
         Serial.println("Roar 1");
-        //more track playing code here track001.mp3
+        playTrack("track001.mp3");
         break;
       case 2:
         Serial.println("Roar 2");
-        //more track playing code here track002.mp3
+        playTrack("track002.mp3");
         break;
       case 3:
         Serial.println("Roar 3");
-        //more track playing code here track003.mp3
+        playTrack("track003.mp3");
         break;
       case 4:
         Serial.println("Roar 4");
-        //more track playing code here track004.mp3
+        playTrack("track004.mp3");
         break;
       default:
-        Serial.println("Random picked a roar that doesn't exist");
+        Serial.println("");
+        Serial.println("choice picked a roar that doesn't exist");
         break;
     }
+  }
+//  } else {
+//    Serial.println("No motion detected");
+//    //if a track is playing stop it
+//    if (MP3player.isPlaying()) {
+//      MP3player.stopTrack();
+//    }
+//  }
+}
+
+/*
+  stop the track if its still playing, play the fileName mentioned, brief error check
+ */
+void playTrack(String fileName) {
+  //if a track is playing stop it
+  if (MP3player.isPlaying()) {
+    MP3player.stopTrack();
+  }
+  //play the named track and check if it works
+  byte result = MP3player.playMP3(fileName);
+  if (result == 0) {
+    Serial.println(fileName + " is playing");
   } else {
-    Serial.println("don't Roar");
-    //if a track is playing stop it
-    if (MP3player.isPlaying()) {
-      MP3player.stopTrack();
-    }
+    Serial.println("Error the track isn't playing for some reason");
+    Serial.println("Error code is " + String(result));
+    /*
+      0 OK
+      1 Already playing track
+      2 File not found
+      3 indicates that the VSdsp is in reset.
+    */
   }
 }
